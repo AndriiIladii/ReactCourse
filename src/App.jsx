bd20ac3e596acd0ed6e9fb9177fd8d77b4226a56
AndriiIladii <iladiy21@gmail.com>
2024-04-18 17:03:38 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import * as styles from "./App.module.css";
+import reactImage from "./public/images/react.png";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello MAN</h1>
+      <p className={styles.text}>text</p>
+      <select>
+        <option value="1">one</option>
+        <option value="2">two</option>
+      </select>
+      <hr />
+      <img src={reactImage} alt="react" />
+    </>
+  );
+};
+
+export default App;
