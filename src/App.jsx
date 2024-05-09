f1843b068e3c4ec68c02ea4cfc9b4aabfc77eae3
AndriiIladii <iladiy21@gmail.com>
2024-05-09 19:43:49 +0300

    lesson 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,15 @@ import { useState } from "react";
 import * as styles from "./App.module.css";
 
 const App = () => {
-  let count4 = 0;
-
-  const [out5Value, setOut5Value] = useState(0);
-  const [output6Value, setOut6Value] = useState(0);
-  const [output8Value, setOut8Value] = useState("");
-  const [output9Value, setOut9Value] = useState(0);
-
-  let div6 = React.createRef();
-  let div7 = React.createRef();
-  let div8 = React.createRef();
-  let div9 = React.createRef();
-  let div10 = React.createRef();
+  const [out5, setOut5] = useState("");
+  const [out6, setOut6] = useState("");
+  const [out7, setOut7] = useState("");
+  const [out8, setOut8] = useState("");
+  const [out9, setOut9] = useState("");
+  const div6 = React.createRef();
+  const div7 = React.createRef();
+  const div9 = React.createRef();
+  const div10 = React.createRef();
 
   function task1() {
     console.log("task2");
@@ -26,45 +23,51 @@ const App = () => {
   function task3(event) {
     console.log(event.target.value);
   }
+
+  let count = 4;
   function task4() {
-    count4++;
-    console.log(count4);
+    console.log(count++);
   }
   function task5(event) {
-    let val = 0;
-    if (event.target.checked) val = event.target.value;
-    setOut5Value(val);
+    let val = event.target;
+    if (val.checked) {
+      setOut5(event.target.value);
+    } else {
+      setOut5(0);
+    }
   }
   function task6() {
-    setOut6Value(inputChange.current.value);
+    setOut6(div6.current.value);
   }
 
   function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.random() * (max - min) + min;
   }
 
   function task7() {
-    div7.current.style.backgroundColor = `rgb(${getRandomInt(
+    let color = `rgb(${getRandomInt(0, 255)}, ${getRandomInt(
       0,
       255
-    )}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`;
+    )}, ${getRandomInt(0, 255)})`;
+    div7.current.style.backgroundColor = color;
+    setOut7(color);
   }
   function task8(event) {
     if (!isNaN(+event.key)) {
-      setOut8Value(output8Value + 1);
+      setOut8(out8 + 1);
     } else {
-      setOut8Value(output8Value + 0);
+      setOut8(out8 + 0);
     }
   }
   function task9() {
-    setOut9Value(div9.current.value);
+    setOut9(div9.current.value);
   }
   let ar10 = [5, 6, 7];
   function task10() {
-    let value = div10.current.value;
-    ar10.push(value);
+    ar10.push(div10.current.value);
     console.log(ar10);
   }
+
   return (
     <>
       <h1>События</h1>
@@ -91,7 +94,7 @@ const App = () => {
       <section>
         <h2>Task 5</h2>
         <input type="checkbox" defaultalue="55" onChange={task5} />
-        <div className="out5">{out5Value}</div>
+        <div className="out5">{out5}</div>
       </section>
       <section>
         <h2>Task 6</h2>
@@ -101,24 +104,21 @@ const App = () => {
           <option value="9">nine</option>
           <option value="10">ten</option>
         </select>
-        <div className="out6">{output6Value}</div>
+        <div className="out6">{out6}</div>
       </section>
       <section>
         <h2>Task 7</h2>
-        <div className={styles.block7} ref={div7}></div>
+        <div className={styles.block7} ref={div7}>
+          {out7}
+        </div>
         <button className="task-7" onClick={task7}>
           Color
         </button>
       </section>
       <section>
         <h2>Task 8</h2>
-        <input
-          type="text"
-          className="task-8"
-          ref={div8}
-          onKeyUp={task8}
-        ></input>
-        <div className="out-8">{output8Value}</div>
+        <input type="text" className="task-8" onKeyUp={task8}></input>
+        <div className="out-8">{out8}</div>
       </section>
       <section>
         <h2>Task 9</h2>
@@ -128,7 +128,7 @@ const App = () => {
           ref={div9}
           onInput={task9}
         ></input>
-        <div className="out-9">{output9Value}</div>
+        <div className="out-9">{out9}</div>
       </section>
       <section>
         <h2>Task 10</h2>
